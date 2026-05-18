837387a21816f148cb0c0c5c822530b770b48775
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-18 09:40:00 +0200

    Titel der App geaendert, es kommt jetzt "Flickr" drin vor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'de.eldecker.ionic.bilderdynamisch',
-  appName: 'BilderDynamischLaden',
+  appName: 'Flickr-Suche',
   webDir: 'www'
 };
 
